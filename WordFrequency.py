3fcffb0dd03d8187683bdd6d4c701482997652cf
Mathieu Glachant <syvwlch@gmail.com>
2018-07-07 18:00:13 -0400

    Get the unique words by tag value

--- WordFrequency.py
+++ WordFrequency.py
@@ -15,12 +15,21 @@ def load_book(filename):
 
 
 def tag_tokens(book):
-    """Tokenize and tag the book's text."""
+    """Tokenize and tag the book."""
     return pos_tag(word_tokenize(book))
+
+
+def unique_words_by_tag(book, tag_value):
+    """Return a set of the words of the given tag value in the book."""
+    nouns = []
+    for word, tag in tag_tokens(book):
+        if tag == tag_value:
+            nouns.append(word)
+    return set(nouns)
 
 
 # main loop
 if __name__ == "__main__":
     # book = load_book('Texts/NonFreeTexts/InfiniteJest.txt')
     book = load_book('Texts/FreeTexts/Hamlet.txt')
-    print(tag_tokens(book))
+    print(unique_words_by_tag(book, 'NN'))
