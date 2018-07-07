9356b6f896893d694aa7635d90b190ca4055f074
Mathieu Glachant <syvwlch@gmail.com>
2018-07-07 18:05:34 -0400

    Count the unique nouns in Hamlet

--- WordFrequency.py
+++ WordFrequency.py
@@ -19,10 +19,10 @@ def tag_tokens(book):
     return pos_tag(word_tokenize(book))
 
 
-def unique_words_by_tag(book, tag_value):
+def unique_words_by_tag(tokens, tag_value):
     """Return a set of the words of the given tag value in the book."""
     nouns = []
-    for word, tag in tag_tokens(book):
+    for word, tag in tokens:
         if tag == tag_value:
             nouns.append(word)
     return set(nouns)
@@ -32,4 +32,6 @@ def unique_words_by_tag(book, tag_value):
 if __name__ == "__main__":
     # book = load_book('Texts/NonFreeTexts/InfiniteJest.txt')
     book = load_book('Texts/FreeTexts/Hamlet.txt')
-    print(unique_words_by_tag(book, 'NN'))
+    tokens = tag_tokens(book)
+    unique_nouns = unique_words_by_tag(tokens, 'NN')
+    print(len(unique_nouns))
