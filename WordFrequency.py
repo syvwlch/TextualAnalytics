0b902bdb45d29724846341d47c3fef720faf5d40
Mathieu Glachant <syvwlch@gmail.com>
2018-07-07 16:41:52 -0400

    Tokenized the book

--- WordFrequency.py
+++ WordFrequency.py
@@ -5,14 +5,20 @@ Will load the text and then create a dictionary of words with the number of
 occurences in the text.
 """
 
+from nltk import word_tokenize
+
 
 def load_book(filename):
     """Load the book's text from the file into memory."""
     with open(filename, 'r') as f:
-        book = f.read()
-    return book
+        return f.read()
+
+
+def tokenize_book(book):
+    """Tokenize a book using word_tokenize function from nltk."""
+    return word_tokenize(book)
 
 
 # main loop
 if __name__ == "__main__":
-    print(load_book('Texts/FreeTexts/hamlet.txt'))
+    print(word_tokenize(load_book('Texts/FreeTexts/hamlet.txt')))
