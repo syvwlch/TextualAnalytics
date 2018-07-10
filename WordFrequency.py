33287b40904838dd1ca91d46b46003d36744656e
Mathieu Glachant <syvwlch@gmail.com>
2018-07-10 18:45:12 -0400

    Cached tagged tokens to disk
    Using pickle module

--- WordFrequency.py
+++ WordFrequency.py
@@ -5,14 +5,34 @@ Will load the text and then create a dictionary of words with the number of
 occurences in the text.
 """
 
+import pickle
 from collections import Counter
 from nltk import pos_tag, word_tokenize
 
 
-def load_book(filename):
-    """Load the book's text from the file into memory."""
-    with open(filename, 'r') as f:
-        return f.read()
+def tokenize_and_tag(filepath):
+    """Load the book's text, tokenize and tag in memory."""
+    with open(filepath+'.txt', 'r') as f:
+        book = f.read()
+    return pos_tag(word_tokenize(book))
+
+
+def pickle_tokens(tokens, filepath):
+    """Pickle the tokens to a file."""
+    with open(filepath+'.pickle', 'wb') as f:
+        pickle.dump(tokens, f)
+    return
+
+
+def unpickle_tokens(filepath):
+    """Unpickle the tokens into memory."""
+    try:
+        with open(filepath+'.pickle', 'rb') as f:
+            tokens = pickle.load(f)
+    except FileNotFoundError:
+        tokens = tokenize_and_tag(filepath)
+        pickle_tokens(tokens, filepath)
+    return tokens
 
 
 def unique_words_by_tag(tokens, tag_value='N/A'):
@@ -30,7 +50,7 @@ def adjective_noun_pairs(tokens):
     last_word = ''
     last_tag = ''
     for word, tag in tokens:
-        if last_tag == 'JJ' and tag == 'NN':
+        if last_tag.startswith('JJ') and tag.startswith('NN'):
             pairs.append(last_word.lower()+' '+word.lower())
         last_word = word
         last_tag = tag
@@ -44,43 +64,39 @@ def adjectives_before(tokens, target_word):
     last_tag = ''
     target_word = target_word.lower()
     for word, tag in tokens:
-        if word.lower() == target_word and last_tag == 'JJ':
+        if word.lower() == target_word and last_tag.startswith('JJ'):
             adjectives.append(last_word.lower())
         last_word = word
         last_tag = tag
     return Counter(adjectives)
 
 
-def words_before(tokens, target_word):
-    """Return a counter of the words which precede the target_word."""
-    words = []
+def nouns_after(tokens, target_word):
+    """Return a counter of the nouns which follow the target_word."""
+    nouns = []
     last_word = ''
     target_word = target_word.lower()
     for word, tag in tokens:
-        if word.lower() == target_word:
-            words.append(last_word.lower())
+        if last_word.lower() == target_word and tag.startswith('NN'):
+            nouns.append(word.lower())
         last_word = word
-    return Counter(words)
-
-
-def words_after(tokens, target_word):
-    """Return a counter of the words which follow the target_word."""
-    words = []
-    last_word = ''
-    target_word = target_word.lower()
-    for word, tag in tokens:
-        if last_word.lower() == target_word:
-            words.append(word.lower())
-        last_word = word
-    return Counter(words)
+    return Counter(nouns)
 
 
 # main loop
 if __name__ == "__main__":
-    # book = load_book('Texts/NonFreeTexts/InfiniteJest.txt')
-    book = load_book('Texts/FreeTexts/Hamlet.txt')
-    tokens = pos_tag(word_tokenize(book))
-    # print(unique_words_by_tag(tokens, 'NN').most_common(20))
-    # print(adjective_noun_pairs(tokens).most_common(20))
-    print(words_after(tokens, 'my'))
-    print(words_before(tokens, 'lord'))
+    PATH = 'Texts/NonFreeTexts/'
+    FILENAME = 'InfiniteJest'
+
+    tokens = unpickle_tokens(PATH+FILENAME)
+
+    print('Most frequent adjectives:')
+    print(unique_words_by_tag(tokens, 'JJ').most_common(5))
+    print('Most frequent nouns:')
+    print(unique_words_by_tag(tokens, 'NN').most_common(5))
+    print('Most frequent adjective/noun pairs:')
+    print(adjective_noun_pairs(tokens).most_common(5))
+    print("Most frequent nouns after the adjective 'little':")
+    print(nouns_after(tokens, 'little').most_common(5))
+    print("Most frequent adjectives before the noun 'way':")
+    print(adjectives_before(tokens, 'way').most_common(5))
